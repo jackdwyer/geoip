b782dd7b79921a1bf9a94260f10e97b9a1bb8005
Jack Dwyer <jackjack.dwyer@gmail.com>
2017-01-13 21:14:02 -0500

    return latitude & longitude

--- README.md
+++ README.md
@@ -2,12 +2,34 @@
 Bash & jq wrapper for MaxMinds GeoIP Demo
 
 ## Example Usage
-`./geoip.sh $(dig +short jackdwyer.org)`
+```bash
+$ ./geoip.sh -h
+Usage: geoip.sh [OPTION] <ip>
 
-`San Francisco | United States | CloudFlare | 104.28.16.112`
+  -h      help
+  -g      lat,lng only
+
+Output Format
+  <state> | <country> | <latitude> | <longitude> | <isp> | <ip>
+
+$ ./geoip.sh $(dig +short jackdwyer.org)
+San Francisco | United States | 37.7697 | -122.3933 | CloudFlare | 104.28.0.37
+
+$ ./geoip.sh -g $(dig +short jackdwyer.org)
+37.7697,-122.3933
+```
 
 ## Dependencies
 [jq](https://stedolan.github.io/jq/)
+
+## Install
+```bash
+$ curl -s -o ${HOME}/bin/geoip https://raw.githubusercontent.com/jackdwyer/geoip/master/geoip.sh
+$ chmod +x ${HOME}/bin/geoip
+```
+
+## TODOs
+[0] More DRY, curl & parsing repeated
 
 ## Thanks
 [MaxMind Geo IP Demo](https://www.maxmind.com/en/geoip-demo)
--- geoip.sh
+++ geoip.sh
@@ -4,6 +4,10 @@
 BASE_URL="https://www.maxmind.com/geoip/v2.1/city/%s?use-downloadable-db=1&demo=1"
 
 check_deps () {
+  if [[ $(command -v curl > /dev/null; echo $?) -ne 0 ]]; then
+    echo "ERROR: missing curl dependency"
+    exit 1
+  fi
   if [[ $(command -v jq > /dev/null; echo $?) -ne 0 ]]; then
     echo "ERROR: missing jq dependency"
     exit 1
@@ -11,7 +15,25 @@ check_deps () {
 }
 
 show_help () {
-  echo "Usage: $(basename "$0") <ip>"
+  SCRIPT_NAME=$(basename "${0}")
+  echo "Usage: ${SCRIPT_NAME} [OPTION] <ip>"
+  echo ""
+  echo "  -h      help"
+  echo "  -g      lat,lng only"
+  echo ""
+  echo "Output Format"
+  echo "<state> | <country> | <latitude> | <longitude> | <isp> | <ip>"
+}
+
+parse_lat_lng() {
+  # TODO do this only once
+  local data="$1"
+  local ip="$2"
+  local lat
+  local lng
+  lat=$(jq '.location.latitude' < "${data}" | sed 's/"//g;')
+  lng=$(jq '.location.longitude' < "${data}" | sed 's/"//g;')
+  printf "%s,%s\n" "${lat}" "${lng}"
 }
 
 parse_loc_data () {
@@ -20,22 +42,51 @@ parse_loc_data () {
   local city=$(jq '.city.names.en' < "${data}" | sed 's/"//g;')
   local country=$(jq '.country.names.en' < "${data}" | sed 's/"//g;')
   local isp=$(jq '.traits.isp' < "${data}" | sed 's/"//g;')
-  printf "%s | %s | %s | %s\n" "$city" "$country" "$isp" "$ip"
+  local lat=$(jq '.location.latitude' < "${data}" | sed 's/"//g;')
+  local lng=$(jq '.location.longitude' < "${data}" | sed 's/"//g;')
+  printf "%s | %s | %s | %s | %s | %s\n" "$city" "$country" \
+                                         "$lat" "$lng" \
+                                         "$isp" "$ip"
+}
+
+do_curl() {
+  local tmp_data;
+  tmp_data=$(mktemp)
+  local ip="$1"
+  curl -s "$(printf "${BASE_URL}" "$ip")" > "${tmp_data}"
+  echo "${tmp_data}"
 }
 
 get_location_data () {
-  local tmp_data=$(mktemp)
-  local ip="$1"
-  curl -s "$(printf "$BASE_URL" "$ip")" > "${tmp_data}"
-  parse_loc_data "${tmp_data}" "$ip"
+  tmp_data=$(do_curl "${1}")
+  parse_loc_data "${tmp_data}" "${1}"
+}
+
+get_geo_data() {
+  tmp_data=$(do_curl "${1}")
+  parse_lat_lng "${tmp_data}" "${1}"
 }
 
 check_deps
 
-if [[ -z $1 ]]; then
-  echo "Error: no IP provided"
+if [[ "${1}" = "-h" ]]; then
   show_help
-  exit 1
+  exit 0
 fi
 
-get_location_data "$1"
+if [[ "${1}" = "-g" ]]; then
+  if [[ -z ${2} ]]; then
+    echo "Error: no IP provided"
+    show_help
+    exit 1
+  fi
+  get_geo_data "${2}"
+else
+  if [[ -z ${1} ]]; then
+    echo "Error: no IP provided"
+    show_help
+    exit 1
+  fi
+  get_location_data "$1"
+fi
+
